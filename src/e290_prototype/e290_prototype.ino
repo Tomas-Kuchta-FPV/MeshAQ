3ce4807447f1146da096ccdba02afdd4468a7bd6
Tomas Kuchta <tomas.kuchta111@gmail.com>
2026-07-03 14:38:12 +0200

    updates

--- src/e290_prototype/e290_prototype.ino
+++ src/e290_prototype/e290_prototype.ino
@@ -33,26 +33,28 @@ SensirionI2cSen66 sensor;
 
 uint8_t padding = 0;
 bool dataReady = false;
-uint16_t massConcentrationPm1p0 = 0;
-uint16_t massConcentrationPm2p5 = 0;
-uint16_t massConcentrationPm4p0 = 0;
-uint16_t massConcentrationPm10p0 = 0;
-int16_t ambientHumidity = 0;
-int16_t ambientTemperature = 0;
-int16_t vOCIndex = 0;
-int16_t nOxIndex = 0;
-uint16_t cO2 = 0;
+float massConcentrationPm1p0 = 0.0;
+float massConcentrationPm2p5 = 0.0;
+float massConcentrationPm4p0 = 0.0;
+float massConcentrationPm10p0 = 0.0;
+float humidity = 0.0;
+float temperature = 0.0;
+float vocIndex = 0.0;
+float noxIndex = 0.0;
+uint16_t co2 = 0;
 float battV = 0;
 
 void setup() {
   Serial.begin(115200);
 
+  //Eink
   Platform::VExtOn();
   display.landscape();
   display.setFont(&FreeSans12pt7b);
   display.printCenter("Starting AQ SEN66");
   display.update();
   
+  //Vbat
   analogReadResolution(12);
   pinMode(46, OUTPUT);
   digitalWrite(46, HIGH);
@@ -60,14 +62,15 @@ void setup() {
 
   delay(50);
 
+  //SEN66
   Wire.begin();
   sensor.begin(Wire, SEN66_I2C_ADDR_6B);
   delay(300);
 
-  error = sensor.stopMeasurement();
+  /* error = sensor.stopMeasurement();
   if (error != NO_ERROR) {
     display.printCenter("Failed to initialize SEN66");
-  }
+  } */
 
   delay(50);
   error = sensor.startContinuousMeasurement();
@@ -75,12 +78,26 @@ void setup() {
     display.printCenter("Failed to initialize SEN66");
   }
 
+  int8_t serialNumber[32] = {0};
+  error = sensor.getSerialNumber(serialNumber, 32);
+  if (error != NO_ERROR) {
+      display.printCenter("Failed to initialize SEN66");
+      Serial.print("Error trying to execute getSerialNumber(): ");
+      errorToString(error, errorMessage, sizeof errorMessage);
+      Serial.println(errorMessage);
+      return;
+  }
+  Serial.print("serialNumber: ");
+  Serial.print((const char*)serialNumber);
+  Serial.println();
+
   if (error == NO_ERROR) {
     display.clearMemory();
     display.printCenter("AQ Started");
     display.update();
   }
   delay(3000);
+  display.setFont(&FreeSans12pt7b); //assure correct font
 }
 
 void goLightSleepFor(uint64_t seconds) {
@@ -129,10 +146,10 @@ void GetValues() {
     return;
   }
   if (dataReady) {
-    error = sensor.readMeasuredValuesAsIntegers(
-        massConcentrationPm1p0, massConcentrationPm2p5,
-        massConcentrationPm4p0, massConcentrationPm10p0, ambientHumidity,
-        ambientTemperature, vOCIndex, nOxIndex, cO2);
+    error = sensor.readMeasuredValues(
+      massConcentrationPm1p0, massConcentrationPm2p5, massConcentrationPm4p0,
+      massConcentrationPm10p0, humidity, temperature, vocIndex, noxIndex,
+      co2);
     if (error != NO_ERROR) {
       dataReady = false;
       return;
@@ -144,19 +161,48 @@ void GetValues() {
 
 void DrawValues() { 
   display.clearMemory();
-  drawRow(5,   125, 22, "Temp", String(ambientTemperature / 200.0), "C");
-  drawRow(5,   125, 45, "PM1",  String(massConcentrationPm1p0 / 10.0));
-  drawRow(5,   125, 68, "PM2.5",String(massConcentrationPm2p5 / 10.0));
-  drawRow(5,   125, 91, "PM4",  String(massConcentrationPm4p0 / 10.0));
-  drawRow(5,   125,114, "PM10", String(massConcentrationPm10p0 / 10.0));
+  drawRow(5,   125, 22, "Temp", String(temperature)/*, "C"*/);
+  drawRow(5,   125, 55, "PM1",  String(massConcentrationPm1p0));
+  drawRow(5,   125, 78, "PM2.5",String(massConcentrationPm2p5));
+  drawRow(5,   125, 101, "PM4",  String(massConcentrationPm4p0));
+  drawRow(5,   125,124, "PM10", String(massConcentrationPm10p0));
   
-  drawRow(160,260,22, "rH",  String(ambientHumidity / 100.0), "%");
-  drawRow(160,260,45, "iVOC", String(vOCIndex));
-  drawRow(160,260,68, "iNOx", String(nOxIndex));
-  drawRow(160,260,91, "CO2", String(cO2)/*, "ppm", true*/);
-  drawRow(160,260,114, "Vbat", String(battV)/*, "ppm", true*/);
+  drawRow(160,260,22, "rH",  String(humidity)/*, "%"*/);
+  drawRow(160,260,55, "iVOC", String((int)round(vocIndex)));
+  drawRow(160,260,78, "iNOx", String((int)round(noxIndex)));
+  drawRow(160,260,101, "CO2", String(co2)/*, "ppm", true*/);
+  drawRow(160,260,124, "Vbat", String(battV)/*, "ppm", true*/);
   
   display.update();
+}
+
+void PrintValues() {
+  Serial.print("massConcentrationPm1p0:");
+  Serial.print(massConcentrationPm1p0);
+  Serial.print(",");
+  Serial.print("massConcentrationPm2p5:");
+  Serial.print(massConcentrationPm2p5);
+  Serial.print(",");
+  /*Serial.print("massConcentrationPm4p0:"); // Arduino serial plotter supports only 8 channels
+  Serial.print(massConcentrationPm4p0);
+  Serial.print(",");*/
+  Serial.print("massConcentrationPm10p0:");
+  Serial.print(massConcentrationPm10p0);
+  Serial.print(",");
+  Serial.print("humidity:");
+  Serial.print(humidity);
+  Serial.print(",");
+  Serial.print("temperature:");
+  Serial.print(temperature);
+  Serial.print(",");
+  Serial.print("vocIndex:");
+  Serial.print(vocIndex);
+  Serial.print(",");
+  Serial.print("noxIndex:");
+  Serial.print(noxIndex);
+  Serial.print(",");
+  Serial.print("co2:");
+  Serial.println(co2);
 }
 
 void loop() {
@@ -168,6 +214,7 @@ void loop() {
     display.printCenter("No data");
     display.update();
   }
+  PrintValues();
   delay(1000);
   goLightSleepFor(28); // sleep ~1 minute
 }
